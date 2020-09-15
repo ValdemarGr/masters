355d4670cb3f7c7ef6afc121bd922a4258dc8e90
Valdemar <randomvald0069@gmail.com>
2020-09-15 21:00:56 +0200

    lex

--- src/root/src/main/scala/compiler/Main.scala
+++ src/root/src/main/scala/compiler/Main.scala
@@ -8,34 +8,36 @@ import tokens.TokenCombinators
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = Blocker[IO].use{ _ =>
-    //val imp = "import std;"
-    //val let = "let a = 22;"
     val program = {
       """
         |import std;
         | //as
         |let a = 22
         |
-        |fun a b c = 22
-        |fun a =
-        |   let b = 22
-        |   let c = 44
-        |   44
-        |;
-        |let c = 'a';
+        |fun a b c = 22;
+        |fun b =
+        |  let c = 22
+        |  42
         |""".stripMargin
     }
 
+    val skip = Set(' ', '\n')
+
     val parsed = fs2.Stream(program)
-      .map(x => TokenCombinators.parser parseOnly x)
+      .map(x => TokenCombinators.parser parseOnly  x)
       .evalMap {
-        case ParseResult.Done(_, result) => IO.pure(result)
+        case ParseResult.Done(rest, result) =>
+          val hasOther: Boolean = rest.collectFirst{ case x if skip.contains(x) => x }.isDefined
+          val o = IO.pure(result)
+          val log = if (hasOther) IO(println(s"Failed with rest ${rest}")) else IO.unit
+          log *> o
         case x => IO(println(s"died at $x")).as(List.empty)
       }
       .flatMap(xs => fs2.Stream(xs: _*))
       .evalTap(x => IO(println(x)))
       .compile
       .drain
+      //.fold(List.empty[Token]){ case (a, b) => a ++ List(b) }
     parsed *> IO(ExitCode.Success)
   }
 }
--- src/root/src/main/scala/tokens/TokenCombinators.scala
+++ src/root/src/main/scala/tokens/TokenCombinators.scala
@@ -7,58 +7,52 @@ import scala.util.matching.Regex
 
 object TokenCombinators {
   import TokenTypes._
+  implicit class ParserTap[+A](p: Parser[A]) {
+    def tap(f: A => Unit): Parser[A] = p.map { value =>
+      f(value)
+      value
+    }
+
+    def ptap(name: String): Parser[A] = tap{ value =>
+      println(s"in ${name} got value ${value}")
+    }
+  }
 
   def regex(r: Regex): Parser[Char] = elem(x => r.findFirstIn(x.toString).isDefined)
 
   val w = skipMany1(whitespace)
   val space = skipMany1(whitespace | char('\n'))
-  val newlineEnd = many(spaceChar) ~> char('\n')
+  val endDecl = many(spaceChar) ~> (char('\n') | char(';'))
   val let = string("let") ~> space
   val fun = string("fun") ~> space
   val id = many1(letter)
   val infixBuiltin = oneOf("+-*/")
-  val `=` = w <~ char('=') <~ w
+  val `=` = w ~ char('=') ~ w
   val `;` = many1(char(';'))
-  val textR = "[a-zA-Z0-9 ,.]".r
+  val textR = "[a-zA-Z0-9 ,.=';]".r
   val text = many(regex(textR))
 
   val number: Parser[Expression] = many1(digit) map ConstantInteger
   val str: Parser[Expression] = bracket(char('\''), text, char('\'')) map ConstantStr
   val expression: Parser[Expression] = number | str
 
-  val imp: Parser[Declaration] = (string("import") ~> w ~> id) map Import
-  val letDecl: Parser[Declaration] = (let ~> id <~ `=`) ~ expression map LetDecl.tupled
-  val funDecl: Parser[Declaration] = (fun ~> (id ~ many(w ~> id)) <~ `=`) ~ (expression || functionBody) map { case ((a, b), c) => FunDecl(a,b,c) }
-  val declaration: Parser[Declaration] = (letDecl | funDecl | imp) <~ (`;` | newlineEnd)
+  val imp: Parser[Declaration] = (string("import") ~> w ~> id) <~ endDecl map Import
+  val letDecl: Parser[Declaration] = (let ~> id <~ `=`) ~ expression <~ endDecl map LetDecl.tupled
+  val funDecl: Parser[Declaration] =
+    (fun ~> (id ~ many(w ~> id)) <~ `=`) ~
+    ((expression <~ endDecl) || functionBody) map { case ((a, b), c) => FunDecl(a,b,c) }
+  val declaration: Parser[Declaration] = letDecl | funDecl | imp
   val comment = string("//") ~> text >| Ignore
   val redundantNl = many1(char('\n'))  >| Ignore
   val emptySpace = skipMany1(whitespace) >| Ignore
 
-  val functionBody = (many(declaration | emptySpace | redundantNl | comment) ~ (expression <~ newlineEnd)) map FunctionBody.tupled
+  val functionBody = (many(
+        declaration
+      | emptySpace
+      | redundantNl
+      | comment
+  ) ~ (expression <~ endDecl)) map FunctionBody.tupled
   val body = many(declaration | emptySpace | redundantNl | comment)
 
   val parser: Parser[List[Declaration]] = token(body)
-
-/*
-  val str: Parser[Constant] = bracket (char('\''), many1(digit | anyChar), char('\'')) map ConstantStr
-  val constant: Parser[Constant] = (many1(digit) map ConstantInteger) | str
-
-  val skipComb = skipMany1(char('\n') | whitespace) >| Ignore
-
-  lazy val body = many(declaration || expression) map Body
-  lazy val funBody: Parser[Body] = braces(body)
-
-  val imp: Parser[Declaration] = (string("import") ~> w ~> id <~ `;`) map Import
-  val letDecl: Parser[Declaration] = (let ~> id <~ `=`) ~ expression <~ `;` map LetDecl.tupled
-  val funDecl: Parser[Declaration] = (fun ~> id ~ many(skipMany(w) ~> id) <~ `=`) ~ (expression | body) <~ `;` map { case ((a, b), c) => FunDecl(a,b,c) }
-  val declaration = letDecl | funDecl | imp | skipComb
-
-  val reference = id map Reference
-  lazy val invocation = many1(expression <~ skipMany(w)) map Invocation
-
-  val primitive = infixBuiltin map InfixBuiltin
-
-  lazy val expression: Parser[Expression] = primitive | constant | reference | invocation
-
-  val parser = token(many(body))*/
 }
