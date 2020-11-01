59fdfba1b01e54d0d8ac2e77d5b1b7e64d4c9fc9
ValdemarGr <randomvald0069@gmail.com>
2020-11-01 02:28:36 +0100

    diffirent lang

--- src/hs/main.hs
+++ src/hs/main.hs
@@ -1,8 +1,3 @@
-main
-  {-= let prog
-          = ((\ main ->
-                ((\ add -> ((main) (main)) (add)))
-                  ((\ main -> (\ add -> (\ a -> (\ b -> ((a) + (b)))))))))
-              ((\ main -> (\ add -> ((((add) (main)) (add)) (10)) (20))))-}
-    let prog = (\add -> add 10 20) (\a -> \b -> a + b) in
+main =
+    let prog = (\main -> ((\add -> main (main) add) ((\a -> (\b -> a + b))))) ((\_ -> (\add -> add 1 2))) in
     putStrLn $ show prog
